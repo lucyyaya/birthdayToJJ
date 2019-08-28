db57c714ab05ea81ec94c6c1645e1d56ff941ed5
lucyyaya <527665282@qq.com>
2019-08-28 15:57:47 +0800

    src

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body {
+  height: 100%;
+  background-color: rgba(238,156,167,.5);
+  font-size: 1em;
+}
+html {
+  font-size: 100%;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+@import './style/common.scss';
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="style-container">
+        <el-row>
+            <el-col :span=24>
+                <a href="#">login</a>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span=24>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>   
+                </ul>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/login.scss";
+</style>
+
